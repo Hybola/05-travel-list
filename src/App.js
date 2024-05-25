a4f876ad0435f38f7f5687fc0232fbba16a58bdc
Hybola <keeratipong.b@gmail.com>
2024-05-25 14:43:44 +0700

    before watch tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 export default function App() {
   return (
-    <>
+    <div className="app">
       <Logo />
-      {/* <Form />
+      <Form />
       <PackingList />
-      <Stats /> */}
-    </>
+      <Stats />
+    </div>
   );
 }
 function Logo() {
-  return <div>Logo</div>;
+  return <h1>Go Taiwan</h1>;
+}
+function Form() {
+  function handleAdd() {}
+  return (
+    <div className="add-form">
+      <span>What do you need for your 😍 trip?</span>
+      <input placeholder="Item..."></input>
+      <button onClick={handleAdd}>add</button>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul className="list">
+        <li></li>
+      </ul>
+      <list className="actions">sort by input order</list>
+    </div>
+  );
+}
+function Stats() {
+  return (
+    <div className="stats">
+      <h1>You have 66 items on your list, and you already packed %</h1>
+    </div>
+  );
+}
+function Item(props) {
+  return;
 }
